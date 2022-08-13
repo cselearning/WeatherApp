ad89a62b728b1e6dc3961512361efabc28965ad5
Prashant Kumar Sharma <62367274+insane-insan@users.noreply.github.com>
2022-08-13 21:58:35 +0530

    hello

--- app/src/main/java/com/example/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -10,9 +10,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.squareup.picasso.Picasso;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -26,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
     RequestQueue requestQueue;
     String jsonURL = "https://api.openweathermap.org/data/2.5/weather?lat=27.900383&lon=78.072281&units=metric&appid=cecfaf87466e5f6516ba6985d3dfef50";
     TextView cityName, temperature, weatherR, windSpeed, sunRise, sunSet;
-    String cityname, temp, weatherr,windspeed;
+    String cityname, temp, weatherr, windspeed;
     long sunrise, sunset;
     ImageView imageView;
     ProgressDialog progressDialog;
@@ -51,45 +52,53 @@ public class MainActivity extends AppCompatActivity {
         requestQueue = Volley.newRequestQueue(this);
 
         StringRequest request = new StringRequest(Request.Method.GET, jsonURL,
-                response -> {
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
 
-                    try {
-                        JSONObject jsonObject = new JSONObject(response);
-
-
-                        JSONObject wind = (jsonObject.getJSONObject("wind"));
-                        windspeed = wind.getString("speed");
-                        windSpeed.setText(windspeed + " m/s");
-
-                        JSONObject weather = jsonObject.getJSONArray("weather").getJSONObject(0);
-                        weatherr = weather.getString("description");
-                        weatherR.setText(weatherr);
-
-                        JSONObject main = jsonObject.getJSONObject("main");
-                        temp = main.getString("temp");
-                        temperature.setText(temp + "°C");
+                        try {
+                            JSONObject jsonObject = new JSONObject(response);
 
 
-                        JSONObject sys = jsonObject.getJSONObject("sys");
-                        sunrise = sys.getLong("sunrise");
-                        sunRise.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunrise * 1000)));
-                        sunset = sys.getLong("sunset");
-                        sunSet.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunset * 1000)));
+                            JSONObject wind = (jsonObject.getJSONObject("wind"));
+                            windspeed = wind.getString("speed");
+                            windSpeed.setText(windspeed + " m/s");
 
-                        cityname = jsonObject.getString("name");
-                        cityName.setText(cityname + " , " + sys.getString("country"));
+                            JSONObject weather = jsonObject.getJSONArray("weather").getJSONObject(0);
+                            weatherr = weather.getString("description");
+                            weatherR.setText(weatherr);
 
-                        progressDialog.cancel();
-
-
-                        String iconcode = weather.getString("icon").trim();
-                        String url = "http://openweathermap.org/img/w/"+iconcode+".png";
+                            JSONObject main = jsonObject.getJSONObject("main");
+                            temp = main.getString("temp");
+                            temperature.setText(temp + "°C");
 
 
-                    } catch (JSONException e) {
+                            JSONObject sys = jsonObject.getJSONObject("sys");
+                            sunrise = sys.getLong("sunrise");
+                            sunRise.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunrise * 1000)));
+                            sunset = sys.getLong("sunset");
+                            sunSet.setText(new SimpleDateFormat("hh:mm a", Locale.ENGLISH).format(new Date(sunset * 1000)));
 
+                            cityname = jsonObject.getString("name");
+                            cityName.setText(cityname + " , " + sys.getString("country"));
+
+                            progressDialog.cancel();
+
+
+                            String iconcode = weather.getString("icon").trim();
+                            String url = "http://openweathermap.org/img/w/" + iconcode + ".png";
+
+
+                        } catch (JSONException e) {
+
+                        }
                     }
-                }, error -> Toast.makeText(MainActivity.this.getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show());
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(MainActivity.this.getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
         requestQueue.add(request);
     }
 }
